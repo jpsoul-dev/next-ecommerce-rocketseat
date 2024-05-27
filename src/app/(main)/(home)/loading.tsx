19c4b25c9bdee41cec4ad7fea5bbee11d3cf81b0
jpsoul-dev <jpsoul-dev@gmail.com>
2024-05-27 17:35:37 -0300

    Loading na Home

--- src/app/(main)/(home)/loading.tsx
+++ src/app/(main)/(home)/loading.tsx
@@ -1,3 +1,9 @@
+import { Skeleton } from "@/components/skeleton";
+
 export default function HomeLoading() {
-  return <div>Carregando...</div>
+  return <div className="grid h-full grid-cols-9  gap-6 grid-rows-6">
+    <Skeleton className="col-span-6 row-span-6 h-[856px]" />
+    <Skeleton className="col-span-3 row-span-3" />
+    <Skeleton className="col-span-3 row-span-3" />
+  </div>
 }
