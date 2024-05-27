d5cbaac25fde20b82be843262e23ac39c7bcef95
jpsoul-dev <jpsoul-dev@gmail.com>
2024-05-27 10:23:51 -0300

    Route Handlers no Next.js

--- src/app/(main)/(home)/page.tsx
+++ src/app/(main)/(home)/page.tsx
@@ -1,4 +1,68 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default async function HomePage() {
-  await new Promise((resolve) => setTimeout(resolve, 2000))
-  return <div>hellow World</div>
+  return (
+    <div className="grid max-h-[860px] grid-cols-9  gap-6 grid-rows-6">
+      <Link
+        href="/"
+        className="relative group col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+      >
+        <Image
+          className="group-hover:scale-105 transition-transform duration-300"
+          src="/moletom-ai-side.png"
+          width={860}
+          height={860}
+          quality={100}
+          alt=""
+        />
+        <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="text-sm truncate">Moleton AI Side</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$129.00
+          </span>
+        </div>
+      </Link>
+
+      <Link
+        href="/"
+        className="relative group col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+      >
+        <Image
+          className="group-hover:scale-105 transition-transform duration-300"
+          src="/moletom-ia-p-devs.png"
+          width={860}
+          height={860}
+          quality={100}
+          alt=""
+        />
+        <div className="absolute bottom-10 right-28 h-10 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="text-sm truncate">Moleton AI Side</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$129.00
+          </span>
+        </div>
+      </Link>
+
+      <Link
+        href="/"
+        className="relative group col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+      >
+        <Image
+          className="group-hover:scale-105 transition-transform duration-300"
+          src="/moletom-java.png"
+          width={860}
+          height={860}
+          quality={100}
+          alt=""
+        />
+        <div className="absolute bottom-10 right-28 h-10 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="text-sm truncate">Moleton AI Side</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$129.00
+          </span>
+        </div>
+      </Link>
+    </div>
+  )
 }
